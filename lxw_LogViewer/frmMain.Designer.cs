03edb6f21f8cda8fb97d5b5aba1a8261ac35548a
lxw <819069052@qq.com>
2020-09-15 10:05:03 +0800

    新增

---
+++ lxw_LogViewer/frmMain.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace lxw_LogViewer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.rtxtShow = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.rtxtShow, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1328, 685);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnOpen);
+            this.panel1.Controls.Add(this.txtFile);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1320, 44);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtFile
+            // 
+            this.txtFile.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtFile.Location = new System.Drawing.Point(3, 8);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(1228, 30);
+            this.txtFile.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnOpen.Location = new System.Drawing.Point(1237, 5);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 36);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "打开";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // rtxtShow
+            // 
+            this.rtxtShow.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.rtxtShow.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtxtShow.ForeColor = System.Drawing.SystemColors.Window;
+            this.rtxtShow.Location = new System.Drawing.Point(4, 55);
+            this.rtxtShow.Name = "rtxtShow";
+            this.rtxtShow.ReadOnly = true;
+            this.rtxtShow.Size = new System.Drawing.Size(1320, 626);
+            this.rtxtShow.TabIndex = 1;
+            this.rtxtShow.Text = "";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1328, 685);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "frmMain";
+            this.Text = "lxw_LogViewer(819069052@qq.com)";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.RichTextBox rtxtShow;
+    }
+}
+
---
+++ lxw_LogViewer/frmMain.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace lxw_LogViewer
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        long Offset = 0;//初始化偏移
+        String fileName = "";
+        private static ConcurrentQueue<String> logQueue = new ConcurrentQueue<String>();
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;//该值确定是否可以选择多个文件
+            dialog.Title = "请选择文件";
+            dialog.Filter = "文本文件(*.log;*.txt)|*.log;*.txt;";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK && dialog.FileName != "")
+            {
+                rtxtShow.Text = "";
+                Offset = 0;
+                fileName = dialog.FileName;
+                txtFile.Text = fileName;
+
+                FileSystemWatcher watcher = new FileSystemWatcher();
+                watcher.Path = fileName.Substring(0, fileName.LastIndexOf("\\") + 1);
+                watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+                watcher.Filter = fileName.Substring(fileName.LastIndexOf("\\") + 1);
+                watcher.Changed += new FileSystemEventHandler(watcher_Changed);
+                watcher.Created += new FileSystemEventHandler(watcher_Created);
+                watcher.Deleted += new FileSystemEventHandler(watcher_Deleted);
+                watcher.Renamed += new RenamedEventHandler(watcher_Renamed);
+                watcher.EnableRaisingEvents = true;
+
+                Thread t = new Thread(new ThreadStart(ShowLog));
+                t.Start();
+
+                LogToQueue();
+            }
+
+        }
+
+        void ShowLog()
+        {
+            while (true)
+            {
+                Thread.Sleep(500);
+                while (logQueue.Count > 0)
+                {
+                    String logInfo = "";
+                    logQueue.TryDequeue(out logInfo);
+                    rtxtShow.Invoke(new Action(() =>
+                       {
+                           if (logInfo != null && logInfo.Contains("ERROR") || logInfo.Contains("error"))
+                           {
+                               //高亮显示
+                               rtxtShow.SelectionStart = rtxtShow.Text.Length;
+                               rtxtShow.SelectionLength = logInfo.Length;
+                               rtxtShow.SelectionColor = Color.FromName("Red");
+                           }
+                           rtxtShow.AppendText(logInfo + "\r\n");
+                       }));
+                }
+            }
+        }
+
+        void watcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            rtxtShow.Invoke(new Action(() =>
+            {
+                rtxtShow.AppendText("文件被重命名");
+            }));
+
+        }
+
+        void watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            rtxtShow.Invoke(new Action(() =>
+            {
+                rtxtShow.AppendText("文件被删除");
+            }));
+
+        }
+
+        void watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            rtxtShow.Invoke(new Action(() =>
+            {
+                rtxtShow.AppendText("文件被创建");
+            }));
+        }
+
+        void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            LogToQueue();
+        }
+
+        private void LogToQueue()
+        {
+            Mutex mutex = new Mutex(false, "mutex");
+            mutex.WaitOne();
+            if (File.Exists(fileName))
+            {
+                using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    if (fs.CanSeek)
+                    {
+                        if (Offset > fs.Length)//防止期间文件删除后创建导致偏移变化
+                        {
+                            Offset = fs.Length - 1;
+                        }
+                        fs.Seek(Offset, SeekOrigin.Begin);
+
+                        byte[] b = new byte[fs.Length - Offset + 1];
+                        int cnt, m = 0;
+                        cnt = fs.ReadByte();
+                        while (cnt != -1)
+                        {
+                            b[m++] = Convert.ToByte(cnt);
+                            cnt = fs.ReadByte();
+                        }
+
+                        List<string> ltInfo = Encoding.UTF8.GetString(b).Split(new string[] { "\r\n" }
+                            , StringSplitOptions.None).ToList();
+
+                        foreach (String item in ltInfo)
+                        {
+                            logQueue.Enqueue(item);
+                        }
+                        Offset = fs.Length; //更新偏移位置
+                    }
+                    else
+                    {
+                        rtxtShow.Invoke(new Action(() =>
+                        {
+                            rtxtShow.AppendText("当前流不支持查找");
+                        }));
+                    }
+                }
+            }
+            mutex.ReleaseMutex();
+        }
+
+    }
+}
